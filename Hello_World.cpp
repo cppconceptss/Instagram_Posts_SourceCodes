de5270ac04f42736151789430b8c66905ece500e
cpp_conceptss <75375310+cppconceptss@users.noreply.github.com>
2020-12-03 17:15:10 +0530

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,11 @@
+// @cpp_conceptss - Instagram_Posts_SourceCodes
+  #include<iostream>
+  #include<conio.h>
+  using namespace std;
+
+  int main()
+  {
+    cout<<"Hello world";
+    getch();
+    return 0;
+  }
