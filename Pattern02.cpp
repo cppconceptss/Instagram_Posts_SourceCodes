74170c11a8cce61a4c632c27bbd0965975c36e44
cpp_conceptss <75375310+cppconceptss@users.noreply.github.com>
2020-12-08 18:21:30 +0530

    Create Pattern02.cpp

---
+++ Pattern02.cpp
@@ -0,0 +1,17 @@
+// @cpp_conceptss - Instagram_Posts_SourceCodes
+#include<iostream>
+using namespace std; 
+
+int main() 
+{ 
+  int i, j; 
+  for(i=5;i>=1;i--) 
+  {
+    for(j=1;j<=i;j++) 
+    { 	
+       cout<<"*"; 
+    } 
+    cout<<endl; 
+  } 
+  return 0; 
+}
