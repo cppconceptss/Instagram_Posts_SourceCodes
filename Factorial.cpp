93ed8ba3441c9035ca5706cd7656890d1ad826dc
cpp_conceptss <75375310+cppconceptss@users.noreply.github.com>
2020-12-11 10:59:06 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,27 @@
+//cpp_conceptss
+#include <iostream>
+using namespace std;
+
+class factorial
+{
+   public:
+   int nstart, fstart,N;
+   factorial()
+   {
+      nstart = 1, fstart= 1;
+      cout << "Enter a number : ";
+      cin>>N;
+   }
+   factorial(factorial &access )
+   { 
+     while ( access.nstart <= access.N) {
+     access.fstart *= access.nstart;
+     ++access.nstart;
+   }
+  cout<<"\nFactorial of "<<access.N <<" = "<< access.fstart;
+};
+int main()
+{
+   factorial ob; factorial access(ob);
+   return 0;
+}
