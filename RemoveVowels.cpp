6a9a70aae3a3d9c42a6c703ec4c3f190ca7498c5
cpp_conceptss <75375310+cppconceptss@users.noreply.github.com>
2020-12-12 10:59:03 +0530

    Create RemoveVowels.cpp

---
+++ RemoveVowels.cpp
@@ -0,0 +1,34 @@
+//cpp_conceptss
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+ int check_vowel(char);
+ int main()
+{
+  char s[100], t[100];
+  int c, d = 0;
+  cout<<"Enter a word : \n";
+  cin>>s;
+  for(c = 0; s[c] != '\0'; c++) 
+  {
+    if(check_vowel(s[c]) == 0)
+    {       
+      t[d] = s[c];
+      d++;
+    }
+  }
+  t[d] = '\0';
+  strcpy(s, t);  
+  cout<<"After removing vowels : "<<s;
+  cout<<"\nRemoved vowels count : "<<c-d;
+  cout<<"\nRemaining count : "<<c-(c-d);
+  return 0;
+}
+ int check_vowel(char ch)
+{
+    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
+      return 1;
+    else
+      return 0;
+}
